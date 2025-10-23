c4f6c5dadd8e635b0dc52d00433e2a0d582e827d
cristurner01 <Cristina_Turner@insideranken.org>
2025-10-23 11:39:14 -0500

    JS Hands on projects ch1

--- JavaScript/data-files/Chapter01/js01/project02/project01-02_txt.js
+++ JavaScript/data-files/Chapter01/js01/project02/project01-02_txt.js
@@ -2,10 +2,12 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Cris Turner
+    Date:   Oct 23, 2025
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
+let service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
--- JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,13 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Cris Turner
+    Date:   Oct 23, 2025
 
-    Filename: project01-04.js
+    Filename: project01-04_txt.js
 */
 
 //define variables for home and work addresses
-
+let homeStreet =  "1 Main St.", homeCity = "Sicilia", homeState = "MA";
+let homeCode = "02103", workStreet = "15 Oak Ln.", workCity = "Central City";
+let workState = "MA", workCode = "02104";
