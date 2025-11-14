6cf19e435bb5d2daab0a32b2f415fa5f32af45e7
Cristina Turner <Cristina_Turner@insideranken.org>
2025-11-14 11:49:59 -0600

    js ch 6 stuff

--- JavaScript/data-files/Chapter06/js06/chapter/js06a_txt.js
+++ JavaScript/data-files/Chapter06/js06/chapter/js06a_txt.js
@@ -4,11 +4,36 @@
       Chapter case
 
       Order Form Code
-      Author: 
-      Date:   
+      Author: Cris Turner
+      Date:   Nov 14, 2025
 
       Filename: js06a.js
  */
+
+window.addEventListener("load", function () {
+      let orderForm = document.forms.orderForm;
+      let model = orderForm.elements.model;
+
+      // Select Model selection list when form opens
+      model.focus();
+
+      // Calculate the cost of the order
+      calcOrder();
+
+      function calcOrder() {
+            //determine the selected model
+            let mIndex = model.selectedIndex;
+            let mValue = model.options[mIndex].value;
+
+            // Determine the selected quantity
+            let qIndex = orderForm.elements.qty.selectedIndex;
+            let quantity = orderForm.elements.qty[qIndex].value;
+
+            // Model cost = model cost times quantity
+            let modelCost = mValue*quantity;
+            orderForm.elements.modelCost.value = modelCost;
+      }
+});
 
 
 
