75b3b222eddf2ad4a60641b11d8576fedb8f24b5
Cristina Turner <Cristina_Turner@insideranken.org>
2025-11-24 11:51:36 -0600

    Update js07_txt.js

--- JavaScript/data-files/Chapter07/js07/chapter/js07_txt.js
+++ JavaScript/data-files/Chapter07/js07/chapter/js07_txt.js
@@ -45,7 +45,43 @@ document.getElementById("getFile").onchange = function() {
             sourceText = sourceText.toLowerCase();
             sourceText = sourceText.trim();
 
-            console.log(sourceText);
+            //leave only alphabet characters and whitespace in the text 
+            let alphaRegx = /[^a-zA-Z\s]/g;
+            sourceText = sourceText.replace(alphaRegx, "");
+
+            // Remove stop words from the text
+            for (let i = 0; i < stopWords.length; i++) {
+                  let stopRegx = new RegExp("\\b"+stopWords[i]+"\\b", "g");
+                  sourceText = sourceText.replace(stopRegx, "");
+            }
+
+            //place the remaining words in array
+            let words = sourceText.split(/\s+/g);
+
+            // Sort the words in alphabetical order
+            words.sort();
+
+            // Create a 2D array in which each item is array
+            // containing a word and its duplicate count
+            let unique = [ [words[0], 1] ];
+            //keep an index of the unique words
+            let uniqueIndex = 0;
+            for (let i = 1; i < words.length; i++) {
+                  if (words[i] === words[i-1]) {
+                        //increase the duplicate count by 1
+                        unique[uniqueIndex] [1]++;
+                  } else {
+                        // Add a new word to the unique array
+                        uniqueIndex++;
+                        unique[uniqueIndex] = [words[i], 1];
+                  }
+            }
+
+            // Sort by descending order of duplicate count
+            unique.sort(byDuplicate);
+            function byDuplicate(a, b) {
+                  return b [1]-a[1];
+            }
       }
 };
 
